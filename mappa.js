70f328ef5a9cb424984ed6d836229838d1f2f913
Marta Agnelli <marta.agnelli15@gmail.com>
2026-03-14 12:48:57 +0100

    Aggiungi nuove icone marker in formato WebP e rimuovi file obsoleti. Aggiorna il CSS e il JavaScript per il caricamento della mappa e del slider. Crea file JSON per le note e modifica il file HTML per integrare le nuove funzionalità.

--- mappa.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([41.5451, 12.3046], 10);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19, 
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
--- pages/osservazioni.js
+++ pages/osservazioni.js
@@ -1,3 +1,98 @@
+// MAPPA ///////////////////
+
+var map = L.map('map').setView([41.91422000958595, 12.512914236630948], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19, 
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+////////////////////////////
+
+
+
+// DATI ////////////////////
+
+function recuperaDati() {
+
+    console.log("Contatto iNat");
+
+    const rAPI = fetch("https://api.inaturalist.org/v1/observations?user_id=marta59293&iconic_taxa=Aves")
+                    .then(response => {return response.json()});
+    const rNote = fetch("note.json")
+                    .then(response => response.json());
+
+    Promise.all([rAPI, rNote])
+        .then( async ([rAPI, rNote]) => {
+
+            const datiSelezionati =  rAPI.results.map(oss => {
+                const nome = oss.species_guess;
+                return {
+                    id: oss.id,
+
+                    nomeScientifico: oss.taxon.name || "Specie sconosciuta",
+                    nomeComune: nome || "Specie sconosciuta",
+                    
+                    cooLon: oss.geojson.coordinates[0],
+                    cooLat: oss.geojson.coordinates[1],
+                    
+                    data: oss.observed_on,
+                    luogo: oss.place_guess,
+                    note: rNote[nome] || "//"
+                };
+            });
+
+            const giaOss = new Set();
+            
+            const datiSelezionati_noRipe = datiSelezionati.filter(oss => {
+                if (giaOss.has(oss.nomeComune)) return false;
+                else {
+                    giaOss.add(oss.nomeComune);
+                    return true;
+                }
+            });
+
+            for (const oss of datiSelezionati_noRipe) {
+                const percorsoIcona = `../immagini/icone_marker_mappa/${oss.nomeComune.toLowerCase().replaceAll(" ", "_")}.webp`;
+                let percorsoFinale;
+
+                try{
+                    const response = await fetch(percorsoIcona, { method: 'HEAD' });
+                    percorsoFinale = response.ok ? percorsoIcona : "../immagini/icone_marker_mappa/default.svg";
+                }
+                catch (e) {
+                    percorsoFinale = "../immagini/icone_marker_mappa/default.svg";
+                }
+
+                var birdIcon = L.icon({
+                    iconUrl: percorsoFinale,
+
+                    iconSize: [70,70],
+                    iconAnchor: [35,35],
+                    popupAnchor: [0, -35],
+                    className: 'birdIcon'
+                });
+
+                L.marker([oss.cooLat, oss.cooLon], {icon : birdIcon})
+                    .addTo(map)
+                    .bindPopup(`<b>${oss.nomeComune}</b>`);
+            };
+
+        })
+        .catch(errore => {
+            console.error("Errore nel recupero dati: ", errore);
+        });
+
+}
+
+recuperaDati()
+
+////////////////////////////
+
+
+
+// SLIDER //////////////////
+
 const sliders = document.querySelectorAll('.contenitore-slider');
 
 sliders.forEach(slider => {
@@ -105,3 +200,4 @@ sliders.forEach(slider => {
 
 });
 
+////////////////////////////
